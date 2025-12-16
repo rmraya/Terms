5bf3eb48404d6209af8ae58b66a837f29eb8b0e4
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-12-16 11:52:58 -0300

    Added Spanish localization

--- src/com/maxprograms/terms/Constants.java
+++ src/com/maxprograms/terms/Constants.java
@@ -17,5 +17,5 @@ public class Constants {
     }
     
     public static final String VERSION = "1.0.0";
-    public static final String BUILD = "20251216_0849";
+    public static final String BUILD = "20251216_1152";
 }
