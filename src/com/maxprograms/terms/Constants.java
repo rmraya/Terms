288245ec35076414639db566dcf3168b291f94ca
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-01-01 06:42:52 -0300

    Capitalized result labels and adjusted copyright year

--- src/com/maxprograms/terms/Constants.java
+++ src/com/maxprograms/terms/Constants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 - 2025 Maxprograms.
+ * Copyright (c) 2024 - 2026 Maxprograms.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 1.0 which accompanies this distribution,
@@ -17,5 +17,5 @@ public class Constants {
     }
     
     public static final String VERSION = "1.0.0";
-    public static final String BUILD = "20251229_0616";
+    public static final String BUILD = "20260101_0641";
 }
